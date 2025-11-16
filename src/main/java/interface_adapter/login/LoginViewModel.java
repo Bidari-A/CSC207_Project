9beb0b2181471b7ce7effbd97841e660e18d0f08
cdcsccc <468027952@qq.com>
2025-11-16 13:07:12 -0500

    Finish delete the cancel button on signup view and make the cencel button on the login view useful

--- src/main/java/interface_adapter/login/LoginViewModel.java
+++ src/main/java/interface_adapter/login/LoginViewModel.java
@@ -1,6 +1,7 @@
 package interface_adapter.login;
 
 import interface_adapter.ViewModel;
+import java.beans.PropertyChangeListener;
 
 /**
  * The View Model for the Login View.
@@ -12,4 +13,18 @@ public class LoginViewModel extends ViewModel<LoginState> {
         setState(new LoginState());
     }
 
+    @Override
+    public void setState(LoginState state) {
+        super.setState(state);
+        firePropertyChanged();
+    }
+
+    public void firePropertyChanged() {
+        super.firePropertyChange("state");
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        super.addPropertyChangeListener(listener);
+    }
 }
