ce71774a4ffe281f0671604f7473b47e44cc1794
Bidari-A <ava.bidari@mail.utoronto.ca>
2025-11-17 00:04:17 -0500

    presenter added, controller updated

--- src/main/java/interface_adapter/create_new_trip/CreateNewTripController.java
+++ src/main/java/interface_adapter/create_new_trip/CreateNewTripController.java
@@ -2,6 +2,8 @@ package interface_adapter.create_new_trip;
 
 import use_case.create_new_trip.CreateNewTripInputBoundary;
 import use_case.create_new_trip.CreateNewTripInputData;
+import interface_adapter.ViewManagerModel;
+
 
 /**
  * The controller for the Create New Trip Use Case.
@@ -9,9 +11,13 @@ import use_case.create_new_trip.CreateNewTripInputData;
 public class CreateNewTripController {
 
     private final CreateNewTripInputBoundary createNewTripInteractor;
+    private final ViewManagerModel viewManagerModel;
 
-    public CreateNewTripController(CreateNewTripInputBoundary createNewTripInteractor) {
-        this.createNewTripInteractor = createNewTripInteractor;
+
+    public CreateNewTripController(ViewManagerModel viewManagerModel) {
+        this.viewManagerModel = viewManagerModel;
+        //this.createNewTripInteractor = createNewTripInteractor;
+        this.createNewTripInteractor = null; // will set later when you implement the use case
     }
 
     /**
@@ -20,10 +26,25 @@ public class CreateNewTripController {
      * @param to the destination
      * @param date the date of the trip
      */
+
     public void execute(String from, String to, String date) {
+        if (createNewTripInteractor == null) {
+            System.out.println("CreateNewTripInteractor not set yet");
+            return;
+        }
+
         final CreateNewTripInputData inputData =
                 new CreateNewTripInputData(from, to, date);
 
         createNewTripInteractor.execute(inputData);
     }
+
+
+
+
+    public void openForm() {
+        viewManagerModel.setState("create new trip");
+        viewManagerModel.firePropertyChange();
+    }
+
 }
--- src/main/java/use_case/create_new_trip/CreateNewTripInputBoundary.java
+++ src/main/java/use_case/create_new_trip/CreateNewTripInputBoundary.java
@@ -1,3 +1,4 @@
+
 package use_case.create_new_trip;
 
 public interface CreateNewTripInputBoundary {
