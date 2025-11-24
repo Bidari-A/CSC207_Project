c93017089e3f84a4a7afc7bc291cd6f2acda4905
cdcsccc <468027952@qq.com>
2025-11-23 22:51:32 -0500

    add the model for delete trip operation

--- src/main/java/use_case/delete_trip_list/DeleteTripInputData.java
+++ src/main/java/use_case/delete_trip_list/DeleteTripInputData.java
@@ -1,4 +1,16 @@
 package use_case.delete_trip_list;
 
 public class DeleteTripInputData {
+
+    private final String tripName;
+    private final String username;
+
+    public DeleteTripInputData(String tripName, String username) {
+        this.tripName = tripName;
+        this.username = username;
+    }
+
+    public String getTripName() { return tripName; }
+
+    public String getUsername() { return username; }
 }
