23fdc3da212ba8136fde035ab4fedd38ff999f29
cdcsccc <468027952@qq.com>
2025-11-23 22:52:26 -0500

    implement fake delete logic in interactor

--- src/main/java/use_case/delete_trip_list/DeleteTripInteractor.java
+++ src/main/java/use_case/delete_trip_list/DeleteTripInteractor.java
@@ -1,4 +1,25 @@
 package use_case.delete_trip_list;
 
-public class DeleteTripInteractor {
+import entity.Trip;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteTripInteractor implements DeleteTripInputBoundary {
+
+    private final DeleteTripOutputBoundary presenter;
+
+    public DeleteTripInteractor(DeleteTripUserDataAccessInterface dao,
+                                DeleteTripOutputBoundary presenter) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void delete(DeleteTripInputData data) {
+        String username = data.getUsername();
+
+        // After deleting the only sample "sigma", list becomes empty.
+        List<Trip> updatedTrips = new ArrayList<>();
+
+        presenter.prepareSuccessView(new DeleteTripOutputData(updatedTrips, username));
+    }
 }
