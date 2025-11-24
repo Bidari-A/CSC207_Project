e475fbe680c9f38e09600c06e8a000d9585dca7a
cdcsccc <468027952@qq.com>
2025-11-23 22:48:58 -0500

    Verify the file unchanged and compatible with delete trip flow

--- src/main/java/interface_adapter/trip_list/TripListState.java
+++ src/main/java/interface_adapter/trip_list/TripListState.java
@@ -8,30 +8,20 @@ public class TripListState {
     private String username = "";
     private String error = null;
 
-    public TripListState(TripListState copy) {
-        trips = copy.trips;
-        username = copy.username;
-        error = copy.error;
-    }
-    public TripListState() {
+    public TripListState() {}
 
+    public TripListState(TripListState copy) {
+        this.trips = copy.trips;
+        this.username = copy.username;
+        this.error = copy.error;
     }
-    public List<Trip> getTrips() {
-        return trips;
-    }
-    public void setTrips(List<Trip> trips) {
-        this.trips = trips;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getError() {
-        return error;
-    }
-    public void setError(String error) {
-        this.error = error;
-    }
+
+    public List<Trip> getTrips() { return trips; }
+    public void setTrips(List<Trip> trips) { this.trips = trips; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getError() { return error; }
+    public void setError(String error) { this.error = error; }
 }
