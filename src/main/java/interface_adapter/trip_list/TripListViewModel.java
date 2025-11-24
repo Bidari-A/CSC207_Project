719379ca36d9ac88001fc3044cfab2343d3ebe50
cdcsccc <468027952@qq.com>
2025-11-23 22:50:00 -0500

    comfirm it compatible with delete trip updates

--- src/main/java/interface_adapter/trip_list/TripListViewModel.java
+++ src/main/java/interface_adapter/trip_list/TripListViewModel.java
@@ -3,10 +3,10 @@ package interface_adapter.trip_list;
 import interface_adapter.ViewModel;
 
 /**
- * The View Model for the Trip List View.
+ * ViewModel for Trip List View.
  */
-
 public class TripListViewModel extends ViewModel<TripListState> {
+
     public TripListViewModel() {
         super("trip list");
         setState(new TripListState());
