030ec057ac0352ecf790399a6ba4da6db612cd47
cdcsccc <468027952@qq.com>
2025-11-29 18:12:03 -0500

    Change the interface adapter and the GUI to make the delete button methods and make it rigth for using the database

--- src/main/java/interface_adapter/trip_list/TripListState.java
+++ src/main/java/interface_adapter/trip_list/TripListState.java
@@ -4,33 +4,39 @@ import entity.Trip;
 import java.util.List;
 
 public class TripListState {
+
     private List<Trip> trips = null;
     private String username = "";
-    private String error = null;
+    private String error = "";
+
+    public TripListState() {}
 
     public TripListState(TripListState copy) {
-        trips = copy.trips;
-        username = copy.username;
-        error = copy.error;
+        this.trips = copy.trips != null ? List.copyOf(copy.trips) : null;
+        this.username = copy.username;
+        this.error = copy.error;
     }
-    public TripListState() {
 
-    }
     public List<Trip> getTrips() {
         return trips;
     }
+
     public void setTrips(List<Trip> trips) {
-        this.trips = trips;
+        this.trips = (trips != null ? List.copyOf(trips) : null);
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getError() {
         return error;
     }
+
     public void setError(String error) {
         this.error = error;
     }
--- src/main/java/interface_adapter/trip_list/TripListViewModel.java
+++ src/main/java/interface_adapter/trip_list/TripListViewModel.java
@@ -5,10 +5,20 @@ import interface_adapter.ViewModel;
 /**
  * The View Model for the Trip List View.
  */
-
 public class TripListViewModel extends ViewModel<TripListState> {
+
+    public static final String VIEW_NAME = "trip list";
+
     public TripListViewModel() {
-        super("trip list");
+        super(VIEW_NAME);
         setState(new TripListState());
+    }
+
+    /**
+     * Convenience method for presenters to update the state and notify listeners.
+     */
+    public void updateState(TripListState newState) {
+        setState(new TripListState(newState)); // deep copy
+        firePropertyChange();
     }
 }
