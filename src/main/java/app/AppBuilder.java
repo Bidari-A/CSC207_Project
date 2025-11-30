209c1dd1877168c6c39eadabb7827cc046200f67
jayla-12 <jaylazhlee@gmail.com>
2025-11-29 19:46:24 -0500

    Add Complete Trip to main and appbuilder

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -37,6 +37,7 @@ import interface_adapter.trip.TripViewModel;
 import interface_adapter.trip_list.TripListController;
 import interface_adapter.trip_list.TripListPresenter;
 import interface_adapter.trip_list.TripListViewModel;
+import use_case.complete_trip.TripDataAccessInterface;
 import use_case.create_new_trip.CreateNewTripInputBoundary;
 import use_case.create_new_trip.CreateNewTripInteractor;
 import use_case.create_new_trip.CreateNewTripOutputBoundary;
@@ -66,7 +67,13 @@ import view.TripListView;
 import view.TripResultView;
 import view.TripView;
 import view.ViewManager;
+import interface_adapter.complete_trip.CompleteTripController;
+import interface_adapter.complete_trip.CompleteTripPresenter;
+import interface_adapter.complete_trip.CompleteTripViewModel;
 
+import use_case.complete_trip.CompleteTripInputBoundary;
+import use_case.complete_trip.CompleteTripInteractor;
+import use_case.complete_trip.CompleteTripOutputBoundary;
 
 
 public class AppBuilder {
@@ -112,6 +119,7 @@ public class AppBuilder {
     private TripListViewModel tripListViewModel;
     private FlightSearchViewModel flightSearchViewModel;
     private HotelSearchViewModel hotelSearchViewModel;
+    private CompleteTripViewModel completeTripViewModel;
 
 
     // NEW: Trip result VM and view
@@ -282,6 +290,25 @@ public class AppBuilder {
         final DeleteCurrentTripController deleteCurrentTripController =
                 new DeleteCurrentTripController(deleteCurrentTripInteractor);
         loggedInView.setDeleteCurrentTripController(deleteCurrentTripController);
+        return this;
+    }
+
+    public AppBuilder addCompleteTripUseCase() {
+
+        completeTripViewModel = new CompleteTripViewModel();
+
+        final CompleteTripOutputBoundary completeTripPresenter =
+                new CompleteTripPresenter(completeTripViewModel);
+
+        final CompleteTripInputBoundary completeTripInteractor =
+                new CompleteTripInteractor((TripDataAccessInterface) tripDataAccessObject, completeTripPresenter);
+
+        final CompleteTripController completeTripController =
+                new CompleteTripController(completeTripInteractor);
+
+        // Give controller to TripView (because completion happens from Trip Detail page)
+        tripView.setCompleteTripController(completeTripController);
+
         return this;
     }
 
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -21,6 +21,7 @@ public class Main {
                 .addTripView()
                 .addLoadTripDetailUseCase()
                 .addDeleteCurrentTripUseCase()
+                .addCompleteTripUseCase()
                 .build();
 
 
